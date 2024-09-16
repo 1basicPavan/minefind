879cde5f416ca73e33ae5a975677753ec111abae
Gustavo Pavan <gustavopavan07@outlook.com>
2024-09-16 16:29:17 -0300

    Add files via upload
    update

--- src/components/skin-preview.component.jsx
+++ src/components/skin-preview.component.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactSkinview3d from "react-skinview3d";
-import Skin from '../assets/skin.png';
+// import React from "react";
+// import ReactSkinview3d from "react-skinview3d";
+// import Skin from 'https://mineskin.eu/skin/Eiki';
 
 
-export const SKinPreview = (props) => {
-    return(
-        <ReactSkinview3d
-        skinUrl={Skin}
-        height="500"
-        width="500"
-      />
-    )
-}  
+// export const SKinPreview = (props) => {
+//     return(
+//         <ReactSkinview3d
+//         skinUrl={Skin}
+//         height="500"
+//         width="500"
+//       />
+//     )
+// }  
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,42 +1,74 @@
-import React, { useEffect, useState } from "react";
-import input from "../components/input";
-import Card from "../components/card";
-import { SKinPreview } from "../components/skin-preview.component";
-import axios from "axios";
+import React, { useState } from "react";
+import ReactSkinview3d  from "react-skinview3d";
+import { WalkingAnimation } from "skinview3d";
 
 function Home() {
-    const [data, setData] = useState();
-    const [value, setValue] = useState();
+    const [value, setValue] = useState('');
+    const [skinUrl, setSkinUrl] = useState('');
 
-    const fetchData = async () => {
-        if(value){
-            try{
-               await axios.get(`https://api.mojang.com/users/profiles/minecraft/${value}`, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-               })
-                .then((res) => {
-                    console.log(res);
-                });
-            }catch(err){
-                console.log(`Error no home.jsx/fetchData`, err);
+    const fetchData = () => {
+        const url = `https://mineskin.eu/skin/${value}`;
+        setSkinUrl(url);
+    };
+
+    const downloadSkin = async () => {
+        if (!skinUrl) return;
+
+        try {
+            const response = await fetch(skinUrl);
+            if (!response.ok) {
+                throw new Error('Network response was not ok.');
             }
-        }else{
-            window.alert("O campo não pode estar vazio");
+            const blob = await response.blob();
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.href = url;
+            a.download = `${value}-skin.png`;
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            window.URL.revokeObjectURL(url);
+        } catch (error) {
+            console.error('Error downloading the skin:', error);
         }
-    }
-    
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter') {
+            fetchData();
+        }
+    };
+
     return (
-        <div class="Main">
-            <div class="Title">MineFind</div>
-            <input className="inputdev" onChange={(e) => setValue(e.target.value)}/>
-            <button onClick={() => fetchData()}>
+        <div className="Main">
+            <div className="Title">MineFind</div>
+            <input 
+                className="inputdev"
+                value={value} 
+                onChange={(e) => setValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+                placeholder="Digite o nome da skin"
+            />
+            <button onClick={fetchData}>
                 Encontrar
             </button>
-            <SKinPreview skinUrl="https://s.namemc.com/2d/skin/face.png?id=8ffa8f74bb88979b&scale=4" />
+            {skinUrl && (
+                <>
+                    <ReactSkinview3d 
+                        skinUrl={skinUrl}
+                        height="500"
+                        width="500"
+                        onReady={({ viewer }) => {
+                            viewer.animation = new WalkingAnimation();
+                          }}
+                    />
+                    <button onClick={downloadSkin} className="download-button">
+                        Baixar Skin
+                    </button>
+                </>
+            )}
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
