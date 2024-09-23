b9651af8d390fb8eb8500faa0be165095fc002dc
pavan <gustavopavan993@gmail.com>
2024-09-23 18:27:57 -0300

    alpha 0.0

--- src/style/input.jsx
+++ src/style/input.jsx
@@ -1,6 +1,39 @@
 import { styled} from "styled-components";
 
-export const Input = styled.input`
+export const Foudase = styled.div`
 
+    display: flex;
+  position: relative;
+  width: 300px; /* Ajuste a largura conforme necessário */
+
+   input {
+  width: 100%;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: #75B798;
+  color: #fff;
+  font-size: 16px;
+}
+
+    .input-container input::placeholder {
+  color: #fff;
+}
+
+    .input-container .icon {
+  position: absolute;
+  top: 50%;
+  right: 10px;
+  transform: translateY(-50%);
+  font-size: 20px;
+  color: #fff;
+}
+
+    .input-container .icon-container {
+  background-color: #2e583e;
+  padding: 10px;
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
 
 `
