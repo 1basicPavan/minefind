ff82187916536c43dfecf8959f279e3f20a0b64a
black <welinghtonmarcelo@gmail.com>
2024-09-24 14:33:48 -0300

    card-render

---
+++ src/style/render.jsx
@@ -0,0 +1,27 @@
+import { styled} from "styled-components";
+
+export const Render = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 500PX;
+
+    .card-skinPreview{
+        width: 220px;
+        border-radius: 10px;
+        background-color: #0F5132;
+    }
+
+    .skinPreview{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+    
+    .usuario{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+        
+    }
+`
